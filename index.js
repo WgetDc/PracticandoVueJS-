38f15bb7b8720fddbc9ee8da9081771c9d2ef003
WgetDc <ingcorellana@gmail.com>
2020-04-30 17:30:56 -0400

    Uso de for con v-for y v-bind:key para identificar elementos dentro de un objeto

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ new Vue({
     data(){
         return {
             name: 'Bitcoin',
-            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png'
+            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+            prices: [
+                { day: 'Lunes', value: 8400},
+                { day: 'Martes', value: 8452},
+                { day: 'Miércoles', value: 7500},
+                { day: 'Jueves', value: 9500 },
+                { day: 'Viernes', value: 8460 },
+                { day: 'Sabado', value: 9250 },
+                { day: 'Domingo', value: 8420 }
+            ]
         }
     }
 })
